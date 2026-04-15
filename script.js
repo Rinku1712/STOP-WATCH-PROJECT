719bb8e8881a8d1c59790f94215373bd359e6fe8
Rinku1712 <rohillarinku446@gmail.com>
2026-04-15 12:34:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,31 +7,38 @@ let m = document.getElementById("minutes");
 let s = document.getElementById("seconds");
 let milli = document.getElementById("ms");
 let laps = document.getElementById("laps");
+let progress = document.getElementById("progress");
+
+/* Load saved laps */
+let savedLaps = JSON.parse(localStorage.getItem("laps")) || [];
+savedLaps.forEach(addLapToUI);
+lapCount = savedLaps.length + 1;
 
 function updateDisplay() {
   h.innerText = hr.toString().padStart(2, "0");
   m.innerText = min.toString().padStart(2, "0");
   s.innerText = sec.toString().padStart(2, "0");
   milli.innerText = ms.toString().padStart(2, "0");
+
+  // progress ring (based on seconds)
+  let offset = 565 - (sec / 60) * 565;
+  progress.style.strokeDashoffset = offset;
 }
 
 function stopwatch() {
   ms++;
-
-  if (ms === 100) {
-    ms = 0;
-    sec++;
-  }
-  if (sec === 60) {
-    sec = 0;
-    min++;
-  }
-  if (min === 60) {
-    min = 0;
-    hr++;
-  }
+  if (ms === 100) { ms = 0; sec++; }
+  if (sec === 60) { sec = 0; min++; }
+  if (min === 60) { min = 0; hr++; }
 
   updateDisplay();
+}
+
+/* Add Lap */
+function addLapToUI(time) {
+  let li = document.createElement("li");
+  li.innerText = `Lap ${lapCount}: ${time}`;
+  laps.appendChild(li);
 }
 
 /* Start */
@@ -42,11 +49,15 @@ document.getElementById("start").onclick = () => {
 };
 
 /* Lap */
-document.getElementById("pause").onclick = () => {
+document.getElementById("lap").onclick = () => {
   if (timer !== null) {
-    let li = document.createElement("li");
-    li.innerText = `Lap ${lapCount}: ${h.innerText}:${m.innerText}:${s.innerText}.${milli.innerText}`;
-    laps.appendChild(li);
+    let time = `${h.innerText}:${m.innerText}:${s.innerText}.${milli.innerText}`;
+
+    addLapToUI(time);
+
+    savedLaps.push(time);
+    localStorage.setItem("laps", JSON.stringify(savedLaps));
+
     lapCount++;
   }
 };
@@ -60,5 +71,7 @@ document.getElementById("reset").onclick = () => {
   lapCount = 1;
 
   updateDisplay();
+
   laps.innerHTML = "";
+  localStorage.removeItem("laps");
 };
